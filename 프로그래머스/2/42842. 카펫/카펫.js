f33f3a0d1dd12ccd5dbdf3f0f5bf38a48a865568
kimhansol2 <119918073+kimhansol2@users.noreply.github.com>
2025-06-03 11:13:31 +0900

    [level 2] Title: 카펫, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -1,23 +1,19 @@
 function solution(brown, yellow) {
- 
     const total = brown + yellow;
-    
-    let left = Math.floor(Math.sqrt(total));
-      
-    let right = total/left
-    
-    if ((right - 2) * (left - 2) === yellow) {
-    return [right, left];
+
+    let height = Math.floor(Math.sqrt(total));
+    let width = total / height;
+
+    if ((width - 2) * (height - 2) === yellow) {
+        return [width, height];
     }
-    
-    for (let i = left+1; i <total; i++){
-        if(total%i !== 0) continue;
-        right = total/i;
-        left = i;
-        if ((right-2)* (left-2) === yellow) {
-           return [left, right]
+
+    for (let i = height - 1; i < total; i--) {
+        if (total % i !== 0) continue;
+        height = i;
+        width = total / i;
+        if ((width - 2) * (height - 2) === yellow) {
+            return [width, height];
         }
-      }
-    
-    
+    }
 }
